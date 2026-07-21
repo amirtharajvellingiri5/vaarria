722236c9fff85079ed3e61514138887029bce785
amirtharajvellingiri5 <amirtharaj.v@outlook.com>
2026-07-21 21:23:44 +0530

    admin changes

--- src/constants/categoryAttributes.js
+++ src/constants/categoryAttributes.js
@@ -4,5 +4,15 @@ export const WEIGHTS = ['Light (<500g)', 'Medium (500g-800g)', 'Heavy (>800g)']
 export const LENGTH_TYPES = ['Full Length', 'Ankle Length']
 export const LINING_OPTIONS = ['Yes', 'No']
 export const PRODUCT_TYPES = ['Ready Made', 'Semi-Stitched', 'Unstitched']
-export const TOP_LENGTHS = ['Crop', 'Regular', 'Longline', 'Tunic Length', 'Knee Length', 'Calf Length']
+export const TOP_LENGTHS = ['Crop', 'Regular', 'Longline', 'Tunic Length', 'Knee Length', 'Calf Length', 'Full Length', 'Above Knee']
 export const DUPATTA_SIZES = ['2.25 m', '2.5 m', '2.75 m']
+export const OCCASIONS = ['Casual', 'Festive', 'Wedding', 'Party Wear', 'Formal', 'Daily Wear']
+export const TOP_TYPES = ['Short Top', 'Long Top', 'A-Line Top', 'Straight Top', 'Asymmetric Top']
+export const KURTI_TYPES = [
+  'Straight Cut', 'A-Line', 'Anarkali', 'Angrakha', 'Princess Cut', 'Panelled', 'Flared', 'Empire Line',
+  'High-Low', 'Asymmetric', 'Side Slit', 'Front Slit', 'Shirt Style', 'Jacket Style', 'Cape Style', 'Kaftan',
+  'Indo-Western', 'Tulip', 'C-Cut', 'Tail Cut', 'Layered', 'Overlay', 'Double Layer', 'Peplum', 'Gathered',
+  'Pleated', 'Umbrella Cut', 'Frock Style', 'Gown Style', 'Dhoti Style', 'Pakistani Style', 'Pathani', 'Tunic',
+  'Front Open', 'Button-Down', 'Wrap Style', 'Yoke Design', 'Balloon', 'Balloon Hem', 'Poncho', 'Cold Shoulder',
+  'Off-Shoulder',
+]
